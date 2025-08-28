da82c1eb1c5bb12c3b6c3d878fba8e2626142955
Diego Huanri <diego00fama@gmail.com>
2025-08-28 18:00:31 -0500

    Completando la Logica

--- calculadora_django/calculadora/tests.py
+++ calculadora_django/calculadora/tests.py
@@ -1,1 +1,13 @@
+from django.test import TestCase
+from .calculator import evaluate_expression, CalculatorError
 
+class CalculatorTests(TestCase):
+    def test_precedence(self):
+        self.assertEqual(evaluate_expression("2+3*4"), 14)
+
+    def test_parentheses(self):
+        self.assertEqual(evaluate_expression("(2+3)*4"), 20)
+
+    def test_invalid_expr(self):
+        with self.assertRaises(CalculatorError):
+            evaluate_expression("2+*3")
