08bb951db6201c40c202088244d4d8117b0c5f3e
Jorst-dev <1566731@senati.pe>
2025-08-28 23:18:34 -0500

    views.py

--- calculadora_django/calculadora/views.py
+++ calculadora_django/calculadora/views.py
@@ -1,15 +1,1 @@
-from django.shortcuts import render
-from .calculator import evaluate_expression, CalculatorError
 
-def index(request):
-    result = None
-    error = None
-
-    if request.method == "POST":
-        expr = request.POST.get("expr", "")
-        try:
-            result = evaluate_expression(expr)
-        except CalculatorError as e:
-            error = str(e)
-
-    return render(request, "index.html", {"result": result, "error":error})
